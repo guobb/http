fe8f88080094043366e1fef8a8ffd3292a8afb38
guobb <18576469764@163.com>
2017-02-17 14:28:20 +0800

    data

--- resData.js
+++ resData.js
@@ -1,3 +1,22 @@
 /**
  * Created by apple on 17/2/17.
  */
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    console.log(req.method); //请求的方法
+    console.log(req.url);    //请求的url
+    console.log(req.headers);//请求的头
+
+    res.statusCode = 404;  //调的响应码
+    //设置响应头
+    res.setHeader('Content-Type','text/html; charset=utf-8');
+    res.write('hello');    //写的响应体
+
+    setTimeout(() => {
+        res.write('-node-');
+        res.end('over');
+    },2000);
+});
+
+server.listen(8080, 'localhost');
